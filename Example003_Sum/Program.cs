d16638d9e220e3cee9f846e71a2a29c5e0152b8f
nadezhda <abuziarova.nadezhda@gmail.com>
2023-01-17 21:05:22 +0300

    add randome1

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿double numberA = new Random().Next(1,10); // 1 2 3 4 ..... 9
-Console.WriteLine (numberA);
-double numberB = new Random().Next(1,10);
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ..... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
